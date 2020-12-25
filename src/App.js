2c65431f21c16211177d75b13d34eba276840192
kumarp777804 <kumarp777804@gmail.com>
2020-12-25 22:14:20 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey There! I started creating React App.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://kumarp777804.github.io/fbreact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets get Started
         </a>
       </header>
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfaCavMVNeYHasZvQ9zG_l466XCGq8TAw",
+  authDomain: "startreact-4633d.firebaseapp.com",
+  projectId: "startreact-4633d",
+  storageBucket: "startreact-4633d.appspot.com",
+  messagingSenderId: "107376849485",
+  appId: "1:107376849485:web:d9eb6765d7397a61ea6204",
+  measurementId: "G-WN3P5W96W1"
+};
